d6595867adfc859f28c57efe00429679667de4fc
marv <942931395@qq.com>
2014-06-07 15:26:22 +0800

    mqclient

--- service-src/MqEngine/src/main.cpp
+++ service-src/MqEngine/src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[])
 {
     void *context = zmq_ctx_new();
     void *frontend = zmq_socket(context, ZMQ_ROUTER);
-    int rc =zmq_setsockopt(frontend, ZMQ_IDENTITY, "S",
+    int rc = zmq_setsockopt(frontend, ZMQ_IDENTITY, "S",
                           1);
     cout << rc << endl;
     rc = zmq_bind(frontend, "tcp://127.0.0.1:8888");
@@ -25,14 +25,15 @@ int main(int argc, const char * argv[])
         zmq_msg_t delimiter;
         zmq_msg_t body;
         int more;
+        int ret;
         size_t moreLen = sizeof(more);
         while (1) {
             zmq_msg_init(&remote);
             zmq_msg_recv(&remote, frontend, 0);
             cout << (char*)zmq_msg_data(&remote) << endl;
             cout << zmq_msg_size(&remote) << endl;
-            zmq_getsockopt(frontend, ZMQ_RCVMORE, &more, &moreLen);
-            if(!more)
+            ret = zmq_getsockopt(frontend, ZMQ_RCVMORE, &more, &moreLen);
+            if(ret != 0 || !more)
             {
                 zmq_msg_close(&remote);
                 break;
@@ -42,8 +43,8 @@ int main(int argc, const char * argv[])
             zmq_msg_recv(&delimiter, frontend, 0);
             cout << (char*)zmq_msg_data(&delimiter) << endl;
             cout << zmq_msg_size(&delimiter) << endl;
-            zmq_getsockopt(frontend, ZMQ_RCVMORE, &more, &moreLen);
-            if(!more)
+            ret = zmq_getsockopt(frontend, ZMQ_RCVMORE, &more, &moreLen);
+            if(ret != 0 || !more)
             {
                 zmq_msg_close(&remote);
                 zmq_msg_close(&delimiter);
